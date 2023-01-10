6a5d85ad18c1c11b10cdca0e618c7a32cc7db36f
Pal <palmarti@cisco.com>
2023-01-10 10:28:22 +0100

    Initial Commit

---
+++ NetTest/NetTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetTestApp.swift
+//  NetTest
+//
+//  Created by Pål-Erik Martinsen on 10/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct NetTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
